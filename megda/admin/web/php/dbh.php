acaa76f7a8feb13fbb37e68bf08a2455cff47dd2
megahardtechnologies2025-cloud <megahard.technologies2025@gmail.com>
2025-08-15 18:35:57 +0530

    Update dbh.php

--- megda/admin/web/php/dbh.php
+++ megda/admin/web/php/dbh.php
@@ -1,9 +1,15 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "Megahard@123";
-$dbName = "MegDA";
+//$dbServername = "localhost";
+//$dbUsername = "root";
+//$dbPassword = "Megahard@123";
+//$dbName = "MegDA";
+
+$dbServername = "megda.local";
+$dbUsername = "megdauser";
+$dbPassword = "megdapass";
+$dbName = "megda";
+
 
 $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
 
